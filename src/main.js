1a289cbbda9e47ca496aecf810c8ba37a0e124ea
helloleemo <87022439+helloleemo@users.noreply.github.com>
2025-05-13 11:17:16 +0800

    build(env): add dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
-import './assets/main.css'
-
+// Vue 3
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// Styles
+import './assets/main.css'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// define
+const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// usages
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
