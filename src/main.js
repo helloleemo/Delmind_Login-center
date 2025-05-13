c13f6b9bdfebacdfe9aed1efc987ebeac591e1eb
helloleemo <87022439+helloleemo@users.noreply.github.com>
2025-05-13 11:29:32 +0800

    Revert "build(env): add dependencies"

--- src/main.js
+++ src/main.js
@@ -1,26 +1,11 @@
-// Vue 3
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// Styles
-import './assets/main.css'
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-// define
 const app = createApp(App)
-const vuetify = createVuetify({
-  components,
-  directives,
-})
 
-// usages
 app.use(router)
-app.use(vuetify)
 
 app.mount('#app')
